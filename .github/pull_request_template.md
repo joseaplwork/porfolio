30d03d5d055475606933113877dba85e0b4ed54a
Jose Antonio Paredes Leon <joseaplwork@gmail.com>
2024-04-22 15:20:33 +0200

    feat: update personal introduction component  (#1)
    * feat: update personal introduction component to use personal links instead of project link
    
    * chore: update pull request template with sections for describing the problem, changes made, testing, and notes
    
    * Update pull request template with sections for describing the problem, changes made, testing, and notes

---
+++ .github/pull_request_template.md
@@ -0,0 +1,11 @@
+## Summary of changes made
+
+_Describe the changes that you made._
+
+## Testing
+
+_Describe how you tested the changes._
+
+## Notes
+
+_Anything else you want to mention._
---
+++ src/app/ui/personal-links.component.tsx
@@ -0,0 +1,22 @@
+import Link from 'next/link';
+
+export default function PersonalLinks() {
+  return (
+    <p>
+      <Link
+        className="underline font-bold inline-block mb-5 text-l mr-5"
+        href="projects"
+      >
+        See projects
+      </Link>
+      <a
+        className="underline font-bold inline-block mb-5 text-l"
+        href={process.env.CV_LINK}
+        target="_blank"
+        rel="noreferrer"
+      >
+        Download CV
+      </a>
+    </p>
+  );
+}

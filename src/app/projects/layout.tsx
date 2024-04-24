f891cd467714cd96bcb79db12600fa14c6e2c5f3
Jose Antonio Paredes Leon <joseaplwork@gmail.com>
2024-04-24 13:12:00 +0200

    refactor: update layout and fetch-projects components in the projects directory (#3)

--- src/app/projects/layout.tsx
+++ src/app/projects/layout.tsx
@@ -4,8 +4,8 @@ interface Props {
 
 export default function Loading({ children }: Props) {
   return (
-    <main className="flex h-[100dvh] lg:items-center justify-center">
-      <div className='container mx-auto px-8 py-6"'>{children}</div>
+    <main className="flex min-h-[100dvh] lg:items-center justify-center">
+      <div className="container mx-auto px-8 pt-6 pb-10">{children}</div>
     </main>
   );
 }

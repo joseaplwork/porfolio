8f0b2da7beece80ed14a9aa9903cbb6262b11fde
Jose Antonio Paredes Leon <joseaplwork@gmail.com>
2024-05-02 22:33:29 +0200

    refactor: remove project link temporarily (#4)

--- src/app/ui/personal-links.tsx
+++ src/app/ui/personal-links.tsx
@@ -3,12 +3,12 @@ import Link from 'next/link';
 export default function PersonalLinks() {
   return (
     <p>
-      <Link
+      {/* <Link
         className="underline font-bold inline-block mb-5 text-l mr-5"
         href="projects"
       >
         See projects
-      </Link>
+      </Link> */}
       <Link
         className="underline font-bold inline-block mb-5 text-l"
         href={process.env.CV_LINK || '/'}
